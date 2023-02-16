011bb83d70d284c49b6fc1effbf31653b888d431
carlosreyes <carlosreyes8436@gmail.com>
2023-02-16 14:12:02 -0600

    add Atropos

---
+++ src/components/ContactCard.jsx
@@ -0,0 +1,11 @@
+import Atropos from "atropos/react";
+
+export function ContactCard() {
+  return (
+    <Atropos>
+      <div className="card w-[700px] bg-image h-[400px] rounded-lg bg-white hidden xl:block">
+        <h1 className="text-2xl font-bold text-center" data-atropos-offset="10">CONTACTO</h1>
+      </div>
+    </Atropos>
+  )
+}
